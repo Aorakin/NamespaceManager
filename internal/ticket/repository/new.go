9cd1a881d57a51fdda48430d6ec59ff5c7efea07
boomoioi <n.nanthawisit@mcaiden.com>
2025-12-16 23:50:44 +0700

    fix: order user tickets by creation date in GetUserTickets method

--- internal/ticket/repository/new.go
+++ internal/ticket/repository/new.go
@@ -7,7 +7,7 @@ import (
 
 func (r *TicketRepository) GetUserTickets(userID uuid.UUID) ([]models.Ticket, error) {
 	var tickets []models.Ticket
-	if err := r.db.Where("owner_id = ?", userID).Find(&tickets).Error; err != nil {
+	if err := r.db.Where("owner_id = ?", userID).Order("created_at DESC").Find(&tickets).Error; err != nil {
 		return nil, err
 	}
 	return tickets, nil
