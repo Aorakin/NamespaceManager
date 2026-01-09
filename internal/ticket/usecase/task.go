f6c8963f8be40aae79b8bba24b30330aa92d009a
Nanthakorn <n.nanthawisit@gmail.com>
2026-01-09 19:33:20 +0700

    feat: add logging for ticket requests sent to resource pools

--- internal/ticket/usecase/task.go
+++ internal/ticket/usecase/task.go
@@ -140,6 +140,7 @@ func (u *TicketUsecase) sendTickets(ticketIDs []uuid.UUID) error {
 	}
 
 	for poolID, poolTickets := range ticketsByPool {
+		log.Printf("[SEND TICKETS] sending request to pool %s, with %d tickets", poolID, len(poolTickets))
 		poolURN, err := u.getPoolURN(poolID, poolTickets[0].GlideletURN)
 		if err != nil {
 			return apiError.NewInternalServerError(fmt.Errorf("failed to get pool URN for pool %s: %w", poolID, err))
